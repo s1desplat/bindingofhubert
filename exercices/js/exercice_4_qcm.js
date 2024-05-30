10e71dacbd97796ecc90b9b86c1ac44e2f5886ea
baldash1 <hugo1.baldassin@epitech.eu>
2024-05-30 17:42:37 +0200

    [+] QCM exo 4 fini, petit bug visuel à régler quand le bouton next s'affiche

---
+++ exercices/js/exercice_4_qcm.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    var button = document.querySelector("a.btn");
+    var selects = document.querySelectorAll("select");
+
+    if (button) {
+        button.addEventListener("click", checkAnswers);
+    }
+
+    selects.forEach((select) => {
+        select.addEventListener("change", () => {
+            select.classList.remove(...["valid-answer", "wrong-answer"]);
+        })
+    })
+})
+
+function checkAnswers(e) {
+    e.preventDefault();
+    let answers = document.querySelectorAll("select");
+    let validAnswers = ["a", "c", "a", "c"];
+    let complete = true;
+    let next_section = document.getElementById("next-section");
+
+    answers.forEach((answer, i) => {
+        if (answer.value != validAnswers[i]) {
+            answer.classList.remove("valid-answer");
+            answer.classList.add("wrong-answer");
+            complete = false;
+        }
+        else {
+            answer.classList.remove("wrong-answer");
+            answer.classList.add("valid-answer");
+        }
+    })
+
+    console.log(complete);
+
+    if (complete)
+        next_section.classList.remove("hidden");
+}
