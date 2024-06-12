ba813d13e4ef14492af0d27e69afad677fe9f645
Hugo <hugo1.mondou@epitech.eu>
2024-06-12 13:45:52 +0200

    [+] complete lore for exercice 1

--- exercices/js/exercice_1.js
+++ exercices/js/exercice_1.js
@@ -5,70 +5,91 @@ function addNewBadge(badgeName, lineClass) {
     document.querySelector(lineClass).append(newBadge);
 }
 
-function exerciceButtonSuccess() {
-    addNewBadge('badge-1', '.ligne-1');
-    document.getElementById('section-finish').classList.remove('caché');
+function generateNextButton(href) {
+    const nextButton = document.createElement("a");
+    nextButton.classList.add("btn");
+    nextButton.classList.add("suivant");
+    nextButton.setAttribute("href", href);
+    nextButton.innerHTML = "Suivant &rarr;"
+
+    document.querySelector('nav').appendChild(nextButton);
 }
 
-function exerciceButtonFailure() {
+function exerciceSuccess() {
+    generateNextButton('exercice_2.html');
+    addNewBadge('badge-1', '.ligne-1');
 
+    const successText = document.querySelector('.exercice section p');
+    successText.innerHTML = "<i>Félicitations ! Tu as aidé Hubert le niglo à retrouvé sont rouleau à pattiserie de l'html !<br><br> Clique sur le bouton 'Suivant' afin de poursuivre ton aventure et retrouver plus d'équipement.<br><br><img class='illustration' src='../img/hubert_rouleau.webp'></img>"
+}
+
+function exerciceButtonSuccess() {
+    const section_3 = document.getElementById('section-3');
+    const show = document.getElementById('i2');
+    section_3.classList.remove('caché');
+    show.classList.remove('caché');
+    const titres = section_3.querySelectorAll('button, input, select');
+
+    if (titres.length == 1) {
+        titres.forEach(titre => {
+            if (titre.tagName === 'BUTTON') {
+                document.getElementById('btn3').onclick = function () {
+                    exerciceSuccess();
+                }
+            } else if (titre.tagName === 'INPUT') {
+                exerciceFailure("Ceci est un champ input, tu peux y rentrer du texte, mais ce n'est pas ce qu'on recherche, essayez autre chose !", 'ie3');
+            } else if (titre.tagName === 'SELECT') {
+                exerciceFailure("Ceci est un champ select, actuellement vide tu peux l'utiliser pour faire une selection d'option fixe, mais ce n'est pas ce qu'on recherche, essayez autre chose !", 'ie3')
+            }
+        });
+    } else {
+        exerciceFailure("Je vois ce que vous avez essayé de faire ici ! Essayez avec une seule proposition !", "ie3");
+    }
 }
 
 function exerciceLinkSuccess() {
-    const section_3 = document.getElementById('section-3');
-    section_3.classList.remove('caché');
-    const titres = section_3.querySelectorAll('button, input, select');
-
-    titres.forEach(titre => {
-        if (titre.tagName === 'BUTTON') {
-            document.getElementById('btn3').onclick = function () {
-                exerciceButtonSuccess();
-            }
-        } else if (titre.tagName === 'INPUT') {
-            exerciceLinkFailure("Le titre est trop grand Hubert trouve le titre vertigineux, essayez autre chose !");
-        } else if (titre.tagName === 'SELECT') {
-            exerciceLinkFailure("")
-        }
-    })
-}
-
-function exerciceLinkFailure() {
-
-}
-
-function exerciceTitleSuccess() {
+    const show = document.getElementById('i1');
     const section_2 = document.getElementById('section-2');
+    show.classList.remove('caché');
     section_2.classList.remove('caché');
-    const titres = section_2.querySelectorAll('a, b, i');
-    
-    titres.forEach(titre => {
-        if (titre.tagName === 'A') {
-            exerciceLinkSuccess();
-        } else if (titre.tagName === 'B') {
-            exerciceLinkFailure("Le titre est trop grand Hubert trouve le titre vertigineux, essayez autre chose !");
-        } else if (titre.tagName === 'I') {
-            exerciceLinkFailure("")
-        }
-    });
+    const titres = section_2.querySelectorAll('a, b, i .trou');
+
+    if (titres.length == 1) {
+        titres.forEach(titre => {
+            if (titre.tagName === 'A') {
+                exerciceButtonSuccess();
+            } else if (titre.tagName === 'B') {
+                exerciceFailure("Et non ceci est un texte en gras, essayez autre chose !", "ie2");
+            } else if (titre.tagName === 'I') {
+                exerciceFailure("Et non ceci est un texte en italique, essayer autre chose !", "ie2");
+            }
+        });
+    } else {
+        exerciceFailure("Je vois ce que vous avez essayé de faire ici ! Essayez avec une seule proposition !", "ie2");
+    }
 }
 
-function exerciceTitleFailure(error) {
-    
+function exerciceFailure(error, id) {
+    const errors = document.getElementById(id);
+    errors.innerHTML = error;
+    errors.classList.remove('caché');
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
     const exo_1 = document.getElementById('section-1');
     const titres = exo_1.querySelectorAll('h1, h2, h3, h4, h5, h6');
     
-    titres.forEach(titre => {
-        if (titre.tagName === 'H2') {
-            exerciceTitleSuccess();
-        } else if (titre.tagName === 'H1') {
-            exerciceTitleFailure("Le titre est trop grand Hubert trouve le titre vertigineux, essayez autre chose !");
-        } else if (titre.tagName === 'H3') {
-            exerciceTitleFailure("")
-        }
-    });
-
-
+    if (titres.length === 1) {
+        titres.forEach(titre => {
+            if (titre.tagName === 'H2') {
+                exerciceLinkSuccess();
+            } else if (titre.tagName === 'H1') {
+                exerciceFailure("Le titre est trop grand Hubert trouve le titre vertigineux, essayez autre chose !", 'ie1');
+            } else if (titre.tagName === 'H3') {
+                exerciceFailure("Le titre est trop petit Hubert à oublié ses lunettes il ne pourras pas le lire, essayez autre chose !", 'ie1');
+            }
+        });
+    } else {
+        exerciceFailure("Je vois ce que vous avez essayé de faire ici ! Essayez avec une seule proposition !", "ie1");
+    }
 })
