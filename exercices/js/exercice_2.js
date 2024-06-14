1e5f3db495fc5c2dff8315d5eb53c93576c8d8b9
Hugo <hugo1.mondou@epitech.eu>
2024-06-14 14:17:36 +0200

    [+] exercice 2 fini, exercice 3 en cours de finition

--- exercices/js/exercice_2.js
+++ exercices/js/exercice_2.js
@@ -18,8 +18,17 @@ function generateNextButton(href) {
     console.log(nextButton);
 }
 
-function exerciceError(error) {
+function exerciceSuccess() {
+    generateNextButton('exercice_3.html');
 
+    const successText = document.querySelector('.exercice section p');
+    successText.innerHTML = "<i>Félicitations ! Tu as aidé Hubert le niglo à retrouvé sont crayon rouge du css !<br><br> Clique sur le bouton 'Suivant' afin de poursuivre ton aventure et retrouver plus d'équipement.<br><br><img class='illustration' src='../img/hubert_redsquare.jpeg'></img>"
+}
+
+function exerciceFailure(error, id) {
+    const errors = document.getElementById(id);
+    errors.innerHTML = error;
+    errors.classList.remove('caché');
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
@@ -28,14 +37,18 @@ document.addEventListener("DOMContentLoaded", (e) => {
         const style = window.getComputedStyle(btn1);
         console.log(style.display);
         if (style.display === 'block') {
-            exerciceError("Ah");
+            exerciceFailure("Il semblerait que le bouton soit visible, mais il n'est pas centré, essayez de le placé au centre !", 'ie1');
+        } else if (style.display === 'none') { 
+            exerciceFailure("Il semblerait que le bouton n'est toujours pas visible, essayez autre chose !", "ie1")
         } else if (style.display === 'inline-block') {
             document.getElementById("section-2").classList.remove("caché");
+            document.getElementById('i1').classList.remove("caché");
         }
     }
 
     document.getElementById('btn2').onclick = function () {
-        document.getElementById("section-finish").classList.remove("caché");
+        document.getElementById('i2').classList.remove('caché');
+        exerciceSuccess();
     }
 
 })
