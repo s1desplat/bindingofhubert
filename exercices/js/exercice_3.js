15ef0b8e6da0e38f131bae119cdb935004e129c9
LetEmoliug <simon1.desplat@epitech.eu>
2024-06-17 17:23:02 +0200

    pedago files + ex3 typo fix

--- exercices/js/exercice_3.js
+++ exercices/js/exercice_3.js
@@ -14,8 +14,6 @@ function generateNextButton(href) {
     document.querySelector('nav').appendChild(nextButton);
 
     addNewBadge('badge-3', '.ligne-1');
-
-    console.log(nextButton);
 }
 
 function exerciceSuccess() {
@@ -24,7 +22,7 @@ function exerciceSuccess() {
     generateNextButton('exercice_4.html');
 
     const successText = document.querySelector('.exercice section p');
-    successText.innerHTML = "<i>Félicitations ! Hubert à récupérer tous son équipement et même un snack !<br><br> On peut maintenant partir à l'aventure sans aucun soucis !!<br><br><img class='illustration' src='../img/hubert_snack.webp'></img>"
+    successText.innerHTML = "<i>Félicitations ! Hubert à récupéré tout son équipement et même un snack !<br><br> On peut maintenant partir à l'aventure sans aucun souci !!<br><br><img class='illustration' src='../img/hubert_snack.webp'></img>"
 }
 
 function exerciceFailure(error, id) {
@@ -49,16 +47,16 @@ function section2() {
             if (select.value === "Hubert" && style.borderRadius != "3px") {
                 exerciceSuccess();
             } else if (style.borderRadius == "3px") {
-                exerciceFailure("Ici la selection s'affiche mais on ne peut pas lire ni voir ce qu'on a sélectionner, essayez autre chose !", "ie2");
+                exerciceFailure("Ici la selection s'affiche, mais on ne peut pas lire ni voir ce qu'on a sélectionné. Essayez autre chose !", "ie2");
             } else if (style.display == "none") {
-                exerciceFailure("Comme dans l'exercice 2, le display empêche de pouvoir voir le selecteur, essayez autre chose !", "ie2");
+                exerciceFailure("Comme dans l'exercice 2, le display empêche de pouvoir voir le selecteur. Essayez autre chose !", "ie2");
             } else {
-                exerciceFailure("Il semblerait que vous n'ayez pas selectionner la bonne valeur, essayez autre chose", "ie2");
+                exerciceFailure("Il semblerait que vous n'ayez pas selectionné la bonne valeur. Essayez autre chose !", "ie2");
             }
         } else if (datalist != null) {
             exerciceFailure("Vous avez choisi la datalist, celle-ci comme son nom l'indique permet d'afficher une liste de data et non de pouvoir sélectionner quelque chose, essayez autre chose !", "ie2");
         } else {
-            exerciceFailure("Hubert ne sais pas ce qu'il s'est passé, avec-vous décommentez l'une des deux options de l'html ?", "ie2");
+            exerciceFailure("Hubert ne sait pas ce qu'il s'est passé, avec-vous décommenté l'une des deux options de l'HTML ?", "ie2");
         }
     }
 }
@@ -82,16 +80,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
             } else if (input_style.color === input_style.backgroundColor) {
                 exerciceFailure("On ne peut même pas lire ce qui est écrit ! Essayez autre chose !", "ie1");
             } else if (input.value === "Hubert" && input_style.fontSize != "0px" && input_style.backgroundColor != "#rgb(240, 240, 240") {
-                exerciceFailure("On peut lire le texte et voir la boîte correctement mais je pense qu'on peut faire mieux", "ie1");
+                exerciceFailure("On peut lire le texte et voir la boîte correctement mais je pense qu'on peut faire mieux.", "ie1");
             } else {
                 exerciceFailure("Il semblerait que tu n'as pas rentré la bonne valeur !", 'ie1');
             }
         } else if (textarea != null) {
-            exerciceFailure("Ceci est une zone de texte, ça fonctionne un peu comme l'outil qu'on cherche mais ce n'est pas celui là", "ie1");
+            exerciceFailure("Ceci est une zone de texte, ça fonctionne un peu comme l'outil qu'on cherche mais ce n'est pas celui là.", "ie1");
         } else if (samp != null) {
-            exerciceFailure("Ceci est une zone d'exemple, ou une zone qui permet de faire des citations par exemple, essayez autre chose !", "ie1");
+            exerciceFailure("Ceci est une zone d'exemple, ou une zone qui permet de faire des citations par exemple. Essayez autre chose !", "ie1");
         } else {
-            exerciceFailure("Alors là même Hubert ne sait pas ce qui ne va pas !", "ie1");
+            exerciceFailure("Alors là, même Hubert ne sait pas ce qui ne va pas !", "ie1");
         }
     }    
 })
---
+++ exercices/pedago/exercice_3.md
@@ -0,0 +1,15 @@
+# Contexte
+
+Dernier exercice à trou, celui-ci mélange toutes les notions déjà vues et rajoute un petit peu de complexité.
+
+Laissez les bien tout essayer, ils devraient commencer à être curieux d'eux mêmes à partir de maintenant.
+
+# Objectif
+
+Premier trou :
+    - HTML : Décommenter l'input
+    - CSS : Décommenter la 3è option (font-size: 20, écriture noir sur font gris)
+
+Second trou :
+    - HTML : Décommenter le select
+    - CSS : Décommenter la 3è option (display inline-block et couleurs normales)
